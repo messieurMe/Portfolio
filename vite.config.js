feca8b5bd6804e1b007073b3d6a02d6c1fe52551
messieurMe <messerrolelol@gmail.com>
2025-07-10 15:16:46 +0300

    Trying to fix deployment #1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/',
+    base: 'https://messieurme.github.io/my_portfolio/',
     plugins: [
         react(),
         tailwindcss()
